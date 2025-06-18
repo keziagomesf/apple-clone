f87a7e5974c51c46ae4cdbf56206788e2ecd71bf
keziagomesf <keziagomes.ferre@gmail.com>
2025-06-18 13:05:41 -0300

    git commit -m "chore: initial commit"

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#2997FF',
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: '#101010',
+      },
+    },
+  },
+  plugins: [],
+}
