6be6201262d7f22b42fb6d70156950bc1fce1027
keziagomesf <keziagomes.ferre@gmail.com>
2025-06-18 14:02:49 -0300

    feat: create Highlights component with functionality and styling

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -25,14 +25,14 @@ const VideoCarousel = () => {
   const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video;
 
   useGSAP(() => {
-    // slider animation to move the video out of the screen and bring the next video in
+    // slider animação para mover o vídeo para fora da tela e trazer o próximo vídeo
     gsap.to("#slider", {
       transform: `translateX(${-100 * videoId}%)`,
       duration: 2,
-      ease: "power2.inOut", // show visualizer https://gsap.com/docs/v3/Eases
+      ease: "power2.inOut",
     });
 
-    // video animation to play the video when it is in the view
+    // animação de vídeo para reproduzir o vídeo quando estiver na visualização
     gsap.to("#video", {
       scrollTrigger: {
         trigger: "#video",
@@ -53,16 +53,16 @@ const VideoCarousel = () => {
     let span = videoSpanRef.current;
 
     if (span[videoId]) {
-      // animation to move the indicator
+      // animação para mover o indicador
       let anim = gsap.to(span[videoId], {
         onUpdate: () => {
-          // get the progress of the video
+          // veja o progresso do vídeo
           const progress = Math.ceil(anim.progress() * 100);
 
           if (progress != currentProgress) {
             currentProgress = progress;
 
-            // set the width of the progress bar
+            // definir a largura da barra de progresso
             gsap.to(videoDivRef.current[videoId], {
               width:
                 window.innerWidth < 760
@@ -72,7 +72,7 @@ const VideoCarousel = () => {
                   : "4vw", // laptop
             });
 
-            // set the background color of the progress bar
+            // definir a cor de fundo da barra de progresso
             gsap.to(span[videoId], {
               width: `${currentProgress}%`,
               backgroundColor: "white",
@@ -80,7 +80,7 @@ const VideoCarousel = () => {
           }
         },
 
-        // when the video is ended, replace the progress bar with the indicator and change the background color
+        // quando o vídeo terminar, substitua a barra de progresso pelo indicador e altere a cor de fundo
         onComplete: () => {
           if (isPlaying) {
             gsap.to(videoDivRef.current[videoId], {
@@ -97,7 +97,7 @@ const VideoCarousel = () => {
         anim.restart();
       }
 
-      // update the progress bar
+      // atualizar a barra de progresso
       const animUpdate = () => {
         anim.progress(
           videoRef.current[videoId].currentTime /
@@ -106,10 +106,10 @@ const VideoCarousel = () => {
       };
 
       if (isPlaying) {
-        // ticker to update the progress bar
+        // ticket para atualizar a barra de progresso
         gsap.ticker.add(animUpdate);
       } else {
-        // remove the ticker when the video is paused (progress bar is stopped)
+        // remova o ticket quando o vídeo estiver pausado (a barra de progresso estiver parada)
         gsap.ticker.remove(animUpdate);
       }
     }
